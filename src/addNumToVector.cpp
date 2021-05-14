76d2a51a81fa1d469f38188195b3b7196b338639
jagrutijadhav <jagrutijadhav137@gmail.com>
2021-05-15 01:32:59 +0530

    file 2

--- src/addNumToVector.cpp
+++ src/addNumToVector.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 int main(){
 
-   
+    //intializing variable   
+    int num = 5;
     //intializing the vector
     vector <int> v{1,2,3,4,5,6,7};
 
     for (int i = 0; i < v.size(); i++)
     {   
         //adding value of num to the elements of vector
-        cout << v[i] << " " ; 
+        cout << v[i] + num << " " ; 
     }
      
 }
 
 /*
 output : 
-1 2 3 4 5 6 7
+6 7 8 9 10 11 12
 */
